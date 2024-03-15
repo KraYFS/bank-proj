fb89e3b3f6b89e7e7161847524325fc9d55e8843
KraYFS <grigorievdaniil13@gmail.com>
2024-03-15 16:19:15 +0200

    added side bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
+import SideBar from "./Components/SIdeBar/SideBar";
 
 function App() {
   return (
     <div className="App">
-      
+      <SideBar />
     </div>
   );
 }
